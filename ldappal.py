7d61fc446f21ff3d16ad1ede2412b031e96078f8
Heiko Noordhof <h.e.noordhof@rug.nl>
2015-07-20 17:18:22 +0200

    start experimenting with asyncio

--- ldappal.py
+++ ldappal.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
-import collections
+import logging
+log = logging.getLogger('hko')
+log.setLevel(logging.DEBUG)
+log.addHandler(logging.FileHandler("/tmp/log"))
+
+import asyncio
+import time, datetime
 import urwid
 from notebook import NoteBook
 
@@ -20,28 +26,15 @@ palette = [
 ]
 
 
-class LoadPage(urwid.Pile):
+class TimePage(urwid.Pile):
     def __init__(self):
         self.hidden = True
-        self.procfd = open("/proc/loadavg")
         self.text = urwid.BigText('---', urwid.font.Thin6x6Font())
-        textadapter = urwid.Overlay(self.text, urwid.SolidFill(), 'center', None, 'middle', None)
-
-        self.nbars = 60
-        self.bartop = 3
-        self.bardata = collections.deque([(0,) for tmp in range(self.nbars)], self.nbars)
-        self.bargraph = urwid.BarGraph(
-                ['graph background', 'bar'],
-                ['graph line background', 'graph line bar'],
-                { (1,0): 'bar smooth', },
-            )
-        self.bargraph.set_bar_width(1)
-
-        super().__init__([(10, textadapter), self.bargraph,])
+        textfit = urwid.Overlay(self.text, urwid.SolidFill(), 'center', None, 'middle', None)
+        super().__init__([(10, textfit), urwid.SolidFill('='), urwid.SolidFill('-'),])
 
     def activate(self):
         self.hidden = False
-        self.updategraph()
 
     def deactivate(self):
         self.hidden = True
@@ -49,13 +42,7 @@ class LoadPage(urwid.Pile):
     def update(self, ticks):
         if self.hidden:
             return
-        loadline = self.procfd.readline(128)
-        self.procfd.seek(0)
-        load1m = loadline.split()[0]
-        load1number = float(load1m)
-        self.text.set_text(load1m)
-        self.bardata.append((load1number,))
-        self.updategraph()
+        self.text.set_text("QQleQ")
 
     def updategraph(self):
         # hlines = [1,2]
@@ -65,15 +52,15 @@ class LoadPage(urwid.Pile):
 class MainWindow(urwid.Frame):
     def __init__(self):
         self.header = urwid.AttrMap(urwid.Text(('menu', "Menu")), 'header')
-        self.footer = urwid.AttrMap(urwid.Text(('status', '"slapdash" - ldap server dashboard in text mode')), 'footer')
+        self.footer = urwid.AttrMap(urwid.Text(('status', '"Ldappa" - ldap text mode tool')), 'footer')
 
-        abouttext = urwid.BigText('slapdash', urwid.font.Thin6x6Font())
+        abouttext = urwid.BigText('Ldappa', urwid.font.Thin6x6Font())
         about = urwid.Overlay(abouttext, urwid.SolidFill('/'), 'center', None, 'middle', None)
         self.about = urwid.Padding(about)
-        self.loadpage = LoadPage()
+        self.timepage = TimePage()
 
         pages = [
-                    ('load', self.loadpage),
+                    ('time', self.timepage),
                     ('slapdash', self.about),
                     ('aap', urwid.SolidFill('a')),
                     ('noot', urwid.SolidFill('b')),
@@ -86,47 +73,42 @@ class MainWindow(urwid.Frame):
         self.content = NoteBook(pages)
         super().__init__(self.content, self.header, self.footer)
 
-    def update(self, ticks):
-        # call update() on all widgets
-        self.loadpage.update(ticks)
+
+def clocktick(widget, loop):
+    loop_epoch_secs = loop.time()
+    clock_epoch_secs = time.time()
+
+    looptime = datetime.datetime.fromtimestamp(loop_epoch_secs)
+    clocktime = datetime.datetime.fromtimestamp(clock_epoch_secs)
+
+    log.debug("Loop secs:  {}    Loop time:  {}".format(loop_epoch_secs, looptime))
+    log.debug("Clock secs: {}    Clock time: {}".format(clock_epoch_secs, clocktime))
+
+    # Call again (and again, and ...) 1 minute later
+    loop.call_at(loop_epoch_secs + 60, clocktick, widget, loop)
+    #loop.call_later(60, clocktick, widget, loop)
+    log.debug("---")
+
 
 def handleInput(key):
     if key == 'esc':
         raise urwid.ExitMainLoop()
 
-class Main:
-    def __init__(self, interval=1):
-        self._alarm = None
-        self.clockticks = 0
-        self.interval = interval
 
-        self.widget = MainWindow()
-        self.loop = urwid.MainLoop(self.widget,
-                palette,
-                unhandled_input=handleInput,
-                event_loop=urwid.AsyncioEventLoop()
-                )
-        self.widget.update(self.clockticks)
-        self.startclock()
-        self.loop.run()
+def main():
+    eventloop = asyncio.get_event_loop()
+    widget = MainWindow()
+    eventloop.call_soon(clocktick ,widget, eventloop)
 
-    def startclock(self):
-        self._alarm = self.loop.set_alarm_in(self.interval, self.clocktick)
-
-    def stopclock(self):
-        if self._alarm:
-            self.loop.remove_alarm(self._alarm)
-        self._alarm = None
-
-    def clocktick(self, loop=None, data=None):
-        self.clockticks += 1
-        self.widget.update(self.clockticks)
-        self.startclock()
-
-def handle_key(key):
-    if key in ('q', 'Q'):
-        raise urwid.ExitMainLoop()
+    mainloop = urwid.MainLoop(
+        widget,
+        palette,
+        unhandled_input=handleInput,
+        event_loop=urwid.AsyncioEventLoop(loop=eventloop),
+        )
+    mainloop.run()
+    event_loop.close()
 
 if __name__ == "__main__":
-    main = Main()
+    main()
 
