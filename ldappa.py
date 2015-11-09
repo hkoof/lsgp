68d31a72724d3f7ec3c373a5e4c12d1d63d89f28
Heiko <heiko@heiks.nl>
2015-11-09 23:00:10 +0100

    try async ldap operations with bonsai

--- ldappa.py
+++ ldappa.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 
 import os.path
 from config import Config
@@ -6,26 +7,45 @@ from config import Config
 import bonsai
 
 
+def wait_result(connection, msgid):
+    i = 0
+    result = None
+    while result is None:
+        i += 1
+        result = connection.get_result(msgid)
+    print ("spin wait:", i)
+    return result
+
 def main():
     conf = Config(os.path.expanduser('~/.ldapparc'))['monitor']
 
     client = bonsai.LDAPClient(conf['url'])
     client.set_credentials("SIMPLE", (conf['binddn'], conf['password']))
-    connection = client.connect()
 
-    result = connection.search('cn=Bytes,cn=Statistics,cn=monitor', 0, '', ['+'])
+    connection = bonsai.LDAPConnection(client, is_async=True)
+    msgid = connection.open()
+    result = wait_result(connection, msgid)
+    print ("async connection result:", result)
+
+    msgid = connection.search('cn=Bytes,cn=Statistics,cn=monitor', 0, '', ['+'])
+    result = wait_result(connection, msgid)
+    print (result)
     nBytes = result[0]['monitorCounter'][0]
 
-    result = connection.search('cn=Entries,cn=Statistics,cn=monitor', 0, '', ['+'])
+    msgid = connection.search('cn=Entries,cn=Statistics,cn=monitor', 0, '', ['+'])
+    result = wait_result(connection, msgid)
     nEntries = result[0]['monitorCounter'][0]
     
-    result = connection.search('cn=Total,cn=Connections,cn=monitor', 0, '', ['+'])
+    msgid = connection.search('cn=Total,cn=Connections,cn=monitor', 0, '', ['+'])
+    result = wait_result(connection, msgid)
     totalConnections = result[0]['monitorCounter'][0]
 
-    result = connection.search('cn=Current,cn=Connections,cn=monitor', 0, '', ['+'])
+    msgid = connection.search('cn=Current,cn=Connections,cn=monitor', 0, '', ['+'])
+    result = wait_result(connection, msgid)
     currentConnections = result[0]['monitorCounter'][0]
 
-    result = connection.search('cn=Max File Descriptors,cn=Connections,cn=monitor', 0, '', ['+'])
+    msgid = connection.search('cn=Max File Descriptors,cn=Connections,cn=monitor', 0, '', ['+'])
+    result = wait_result(connection, msgid)
     maxDescriptors = result[0]['monitorCounter'][0]
 
     connection.close()
