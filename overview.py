f939c3751540b73a1b43ce4982814f2442752a74
Heiko Noordhof <h.e.noordhof@rug.nl>
2016-04-18 17:07:56 +0200

    rename widget label to 'search operations'

--- overview.py
+++ overview.py
@@ -8,14 +8,13 @@ class Overview(urwid.Filler):
     def __init__(self, cnmonitor):
         self.cnmonitor = cnmonitor
         self.cnmonitor.subscribe(self.update, "cn=Search,cn=Operations", "monitorOpCompleted", 1)
-        self.connections_label = urwid.Text("Connections:")
-        # self.connections_monitor = urwid.Text('', align='right')
-        self.connections_monitor = urwid.Text('---')
-        self.connections_map1 = urwid.AttrMap(self.connections_monitor, 'value')
-        container1 = urwid.Pile((self.connections_label, self.connections_map1,))
+        self.label = urwid.Text("Search operations:")
+        self.searches_monitor = urwid.Text('')
+        self.searches_map1 = urwid.AttrMap(self.searches_monitor, 'value')
+        container1 = urwid.Pile((self.label, self.searches_map1,))
         super().__init__(container1)
 
     def update(self, value):
         log.debug("update value: {}".format(repr(value)))
-        self.connections_monitor.set_text(str(value))
+        self.searches_monitor.set_text(str(value))
 
