0d68b8383024aac7548f5bb58bb065f23ae4a340
Heiko <heiko@heiks.nl>
2016-04-30 20:37:59 +0200

    experiment with bargraph

--- bars.py
+++ bars.py
@@ -5,16 +5,24 @@ import urwid
 palette = [
     ('normal', 'dark blue', 'dark gray'),
     ('inverse', 'dark gray', 'dark blue'),
+    ('other', 'dark gray', 'light blue'),
+    ('other2', 'light blue', 'dark blue'),
+    ('other3', 'light blue', 'dark gray'),
 ]
 
 def main():
     graph = urwid.BarGraph(
-        ['normal', 'inverse'],
-        ['normal', 'inverse'],
-        { (1,0): 'normal', }, 
+        ['normal', 'inverse', 'other'],
+        ['normal', 'inverse', 'other'],
+        {
+           (1,0): 'normal',
+           (2,0): 'other3',
+           (2,1): 'other2',
+        }, 
     )
-    bardata = [(1,), (2,), (4,), (8,), (16,), (32,)]
-    lines = [10, 20]
+    bardata = [(1,6), (2,5), (4,4), (8,3), (16,2), (32,1)]
+    ## exposes bug in bar 4? ## bardata = [(1,1), (2,2), (4,3), (8,4), (16,5), (32,6)]
+    lines = [3.5, 20]
     graph.set_data(bardata, 40, lines)
     loop = urwid.MainLoop(graph, palette)
     loop.run()
